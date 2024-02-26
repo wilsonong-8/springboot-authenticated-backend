e9c7a4c983d4803888a5b27a0292f51313a9ef37
Wilson Ong <wilsonong.hs@gmail.com>
2024-02-26 15:37:42 +0800

    Added Userservice to checkif user is avalable

---
+++ src/main/java/com/example/AuthenticatedBackend/models/ApplicationUser.java
@@ -0,0 +1,98 @@
+package com.example.AuthenticatedBackend.models;
+
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class ApplicationUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer userId;
+    private String userName;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role_junction",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> authorities;
+
+    public ApplicationUser() {
+        super();
+        this.authorities = new HashSet<Role>();
+    }
+
+    public ApplicationUser(Integer userId, String userName, String password, Set<Role> authorities) {
+        super();
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/AuthenticatedBackend/services/UserService.java
@@ -0,0 +1,32 @@
+package com.example.AuthenticatedBackend.services;
+
+import com.example.AuthenticatedBackend.models.ApplicationUser;
+import com.example.AuthenticatedBackend.models.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        System.out.println("In the user details service");
+        if(!username.equals("Ethan")) throw new UsernameNotFoundException("Not Ethan");
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(1,"USER"));
+
+        return new ApplicationUser(1,"Ethan", encoder.encode("password"),roles );
+    }
+}
